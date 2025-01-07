b745eb3a16a66cc5fee2c759d8aff270d815d33e
Deeppadariya-129 <deeppadariya129@gmail.com>
2025-01-08 01:31:28 +0530

    update

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+// Const 
+const accountId = 7083;
+
+// Let 
+let accountEmail = "deep@gmail.com";
+
+// Var 
+var accountPasswoard = '1234';
+
+
+console.log(accountId); // 7083
+
+// Use instead of console.log
+console.table([accountId , accountEmail , accountPasswoard]); // [ 7083, 'deep@gmail.com', '1234' ]
---
+++ 02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version 
+
+// Data Types in js :- 
+
+// number => 1,2,3,any int
+// bigint => for large number
+// string => ""
+// boolean => true or false 
+// null 
+// undefined
+// Symbol
+// Object
+
+const Name = "Deep"
+const age = 20
+const isAdult = true
+const address = null
+const country = undefined
+
+console.table([Name,age,isAdult,address,country]);
+// console.log([Name,age,isAdult,address,country]); // for array of values
+
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof isAdult);
+console.log(typeof address);
+console.log(typeof country);

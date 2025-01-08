ab326229ddb5f38e82d1a4e035b9daffdb77291d
Deeppadariya-129 <deeppadariya129@gmail.com>
2025-01-08 16:13:33 +0530

    done

---
+++ 03_datatypeConversion.js
@@ -0,0 +1,37 @@
+let Name = "Deep"
+
+// console.log(typeof Name);
+
+let valueInNumber = Number(Name)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33ac" => NaN
+// true => 1 false => 0
+
+let isLoggedIn = undefined
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false 
+// "" => false
+// null => false
+// "Deep" => true
+// undefined => false
+// NaN => false
+
+
+let num = true
+let stringNum = String(num)
+console.log(typeof stringNum);
+console.log(stringNum);
+
+// 33 => "33"
+// 33.33 => "33.33"
+// true => "true"
+// false => "false"
+// null => "null"
+// undefined => "undefined"
+// "[1,2,3]" => "1,2,3"  // stringfy() method is used to convert array to string

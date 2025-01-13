268c006b8249d75df4dafde22312eb48d2f6b611
Deeppadariya-129 <deeppadariya129@gmail.com>
2025-01-13 22:47:00 +0530

    update 06

---
+++ 06_string.js
@@ -0,0 +1,22 @@
+const string = "Hello my name is John" // Dont use this method 
+const stringFinal = `Hello my name is John` // Use this method
+
+
+let name = "John";
+const s1 = `Hello MR. ${name} How are you?`; // Use this method
+console.log(s1);
+
+
+let practiceString = `My name is john And I am SDE`
+
+console.log(practiceString.substring(0,5));
+console.log(practiceString.replace('john' , 'Paul'));
+console.log(practiceString.length);
+console.log(practiceString.charAt(0));
+console.log(practiceString.indexOf('j'));
+
+
+let witeSpace = `    hello world  `
+
+console.log(witeSpace);
+console.log(witeSpace.trim());

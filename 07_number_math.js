a0944f28eefb2f754c5914d08a491ae707a1393a
Deeppadariya-129 <deeppadariya129@gmail.com>
2025-01-13 23:23:15 +0530

    update math

--- 07_number_math.js
+++ 07_number_math.js
@@ -17,3 +17,24 @@ const num4 = 100.24599
 console.log(num4.toPrecision(5));
 
 
+
+// +++++++++++++++++++++++++++ MATH +++++++++++++++++++++++++++
+
+// we need number b/w 10 to 20
+
+
+// Important Math methods 
+// 1.random 
+// 2.floor
+// 3.round
+// 4.cile
+// 5.abs
+// 6.max
+// 7.min
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1)+10));
+
+

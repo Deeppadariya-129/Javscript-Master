2a85ef8144c28318d9a566bffac21d2dc1b24b4d
Deeppadariya-129 <deeppadariya129@gmail.com>
2025-01-13 23:06:03 +0530

    update number part

---
+++ 07_number_math.js
@@ -0,0 +1,19 @@
+const num1 = 100
+
+console.log(num1);
+console.log(typeof num1);
+
+const num2 = new Number(200)
+console.log(num2);
+
+const num3 = 1000000
+
+console.log(num3.toFixed(5));
+console.log(num3.toLocaleString());
+console.log(num3.toLocaleString('en-IN'));
+
+
+const num4 = 100.24599
+console.log(num4.toPrecision(5));
+
+
